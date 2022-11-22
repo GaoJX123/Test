eb0607ae4a2f03b16d43dbfe80b083a09c723726
GaoJX <you@example.com>
2022-11-22 16:58:47 +0800

    commit

--- src/test2.java
+++ src/test2.java
@@ -1,6 +1,15 @@
 public class test2 {
     public static void main(String[] args) {
+        for(int i=0;i<10;i++){
+            System.out.println(i);
+        }
         System.out.println(1);
         System.out.println(2);
+        System.out.println(2);
+        System.out.println(2);
+        System.out.println(2);
+        System.out.println(2);
+        System.out.println(2);
+
     }
 }
